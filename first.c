2d53342775f12e514224c6a93980c43b18214504
abhay shankar <abhayshnkr905@gmail.com>
2025-10-24 22:13:47 +0530

    Add two numbers using third variable

--- first.c
+++ first.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int x,y;
+    int x,y,z;
     printf("Enter a number:");
     scanf("%d%d",&x,&y);
-    printf("The Sum of %d & %d is%d",x,y,x+y);
+    z=x+y;
+    printf("The Sum of %d & %d is:%d",x,y,z);
 
     return 0;
 }
