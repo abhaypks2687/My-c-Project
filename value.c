207905e3148666fac67ee29950c0868be521b51d
abhay shankar <abhayshnkr905@gmail.com>
2025-10-24 22:27:31 +0530

    Value of Variable

---
+++ value.c
@@ -0,0 +1,10 @@
+// WAP to print value of a variable//
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    printf("The value of variable is %d",a);
+    return 0;
+}
