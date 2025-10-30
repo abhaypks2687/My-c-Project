a5630c23edff0b0686940f3357a7df1691195197
abhay shankar <abhayshnkr905@gmail.com>
2025-10-31 00:02:05 +0530

    value of Variable 1.0

--- value.c
+++ value.c
@@ -2,10 +2,18 @@
 #include<stdio.h>
 int main()
 {
-    char m='a';
-    /*int a=20;
+    int a=5,b=6;
+    printf("%d %d\n",a,b);
+    printf("a=%d and b=%d\n",a,b);
+    printf("a=%d and b=%d\n",b,a);
+    printf("value of a %d is and value of b is %d\n",a,b);
+    printf("Sum of %d and %d=%d\n",a,b,a+b);
+    printf("%d+%d=%d",a,b,a+b);
+    /*char m='a';
+    int a=20;
     printf("Enter a number:");
-    scanf("%d",&a);*/
-    printf("The value of variable is %d",m);
+    scanf("%d",&a);
+    printf("The value of variable is %d",m);*/
+
     return 0;
 }
