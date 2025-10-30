c98a5fd3d01223524fb9606448b706ed5040cd33
abhay shankar <abhayshnkr905@gmail.com>
2025-10-30 22:28:05 +0530

    Print Escape Sequence

--- snd.c
+++ snd.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    printf("Welcome \nStudents");
+    printf("\nWelcome Students\n");
+    printf("\tWelcome Students\n");
+    printf("Welcom\be Students\n");
+    printf("Wel\rcome Students\n");
+    printf("\\Welcome Students\n");
+    printf("\"Welcome Students\"\n");
+    printf("\'Welcome Students\'");
     /*printf("\n");
     printf("Students");*/
     return 0;
