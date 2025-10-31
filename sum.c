cefdb9b375cf318b01ac6bff20ac8c474002d11d
abhay shankar <abhayshnkr905@gmail.com>
2025-10-31 22:48:04 +0530

    Sum of two integer 1.0

--- sum.c
+++ sum.c
@@ -2,10 +2,12 @@
 #include<stdio.h>
 int main()
 {
-int a,b;
+int a,b,c;
 printf("Enter First Number:");
 scanf("%d",&a);
 printf("Enter Second Number:");
 scanf("%d",&b);
-printf("Sum of two integers is %d",a+b);
+c=a+b;
+printf("Sum of two integers is %d",c);
+/*printf("Sum of two integers is %d",a+b);*/
 }
