7df4963a8f9b0eb785a29f3e47d4326d95ac1366
abhay shankar <abhayshnkr905@gmail.com>
2025-10-31 22:40:15 +0530

    Sum of two integers

---
+++ sum.c
@@ -0,0 +1,11 @@
+//WAP to program to calualte sum of two integers//
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter First Number:");
+scanf("%d",&a);
+printf("Enter Second Number:");
+scanf("%d",&b);
+printf("Sum of two integers is %d",a+b);
+}
