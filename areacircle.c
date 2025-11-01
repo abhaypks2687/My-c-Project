a9a00d6c3d63f80cf91ed815d67d284a79d24c73
abhay shankar <abhayshnkr905@gmail.com>
2025-11-01 13:19:56 +0530

    Area of Circle and Circumference of circle 1.0

--- areacircle.c
+++ areacircle.c
@@ -2,11 +2,11 @@
 #include<stdio.h>
 int main()
 {
-    float radius,area,perimeter;
+    float radius,area,Circumference;
     printf("Enter radius:");
     scanf("%f",&radius);
     area=3.14*radius*radius;
-    perimeter=2*3.14*radius;
-    printf("Area of Circle is %0.2f\nPerimeter of Circle is %0.2f",area,perimeter);
+    Circumference=2*3.14*radius;
+    printf("Area of Circle is %0.2f\nPerimeter of Circle is %0.2f",area,Circumference);
     return 0;
 }
