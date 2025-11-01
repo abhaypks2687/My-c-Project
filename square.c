1baf23bcaf84a2d882cc3a03da6511e2d8bbd605
abhay shankar <abhayshnkr905@gmail.com>
2025-11-01 10:57:13 +0530

    Squae of a number

---
+++ square.c
@@ -0,0 +1,19 @@
+// WAP to caluclate square of a given number. Number is intered by user.
+#include<stdio.h>
+int main()
+/*{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("Square of a number %d is:%d",num,num*num);
+    system("pause");Windows पर output दिखाने के लिए
+    return 0;
+}*/
+    {
+     int num,x;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    num=x*x;
+    printf("Square of a number %d is:%d",x,num);
+    return 0;
+    }
