02757e0907456faaf9927b7d4f0abc5731195bc5
abhay shankar <abhayshnkr905@gmail.com>
2025-11-01 09:40:07 +0530

    Print Escapen sequence 1.0

--- snd.c
+++ snd.c
@@ -8,6 +8,9 @@ int main()
     printf("\\Welcome Students\n");
     printf("\"Welcome Students\"\n");
     printf("\'Welcome Students\'");
+    printf("\\n\n");
+    printf("\\r\n");
+    printf("\"Teacher's Day\" \'Teachers's Day\'");
     /*printf("\n");
     printf("Students");*/
     return 0;
