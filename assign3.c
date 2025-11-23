f75fa5121ed1b3535a2f20690db7616618c3dcaa
abhay shankar <abhayshnkr905@gmail.com>
2025-11-23 19:11:25 +0530

    Ascii code

---
+++ assign3.c
@@ -0,0 +1,10 @@
+// WAP to input a character from the user and print its ASCII code//
+#include<stdio.h>
+int main()
+{
+    char m;
+    printf("Enter a character:");
+    scanf("%c",&m);
+    printf("ASCII code of input character %c is %d",m,m);
+    return 0;
+}
