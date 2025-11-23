dc29425242212f2b163534590685bc2421db9cdd
abhay shankar <abhayshnkr905@gmail.com>
2025-11-23 22:15:09 +0530

    Ascii code2

--- assign3.c
+++ assign3.c
@@ -9,12 +9,22 @@ int main()
     return 0;
 }*/
 //2. Wap a program to input ASCII code from the user and print its corresponding character//
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     char ch;
     printf("Enter an ASCII code:");
     scanf("%c",&ch);
     printf("ASCII character %c is %d",ch,ch);
+    return 0;
+}*/
+//3.Wap to input three charcter from the user and display character with their corresponding ASCII codes//
+#include<stdio.h>
+int main()
+{
+    char m,ch,a;
+    printf("Enter three characters:");
+    scanf("%c%c%c",&m,&ch,&a);
+    printf("ASCII code is %c %d\nASCII code is %c %d\nASCII code is %c %d",m,m,ch,ch,a,a);
     return 0;
 }
