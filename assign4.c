9721a8bf6fb2bbbad93646645198c4c3c19c9ee9
abhay shankar <abhayshnkr905@gmail.com>
2025-11-24 19:20:27 +0530

    size of char const

---
+++ assign4.c
@@ -0,0 +1,9 @@
+//Wap to calculate of size of character constant//
+/*#include<stdio.h>
+int main()
+{
+    char ch='@';
+    ch=sizeof('@');
+    printf("Size of character constant is:%d",ch);
+    return 0;
+}*/
