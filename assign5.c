c312b1a617376709c48856a531fd8d6a0cc4b52d
abhay shankar <abhayshnkr905@gmail.com>
2025-12-10 22:22:28 +0530

    Appendd a digit in the number

--- assign5.c
+++ assign5.c
@@ -16,8 +16,8 @@ int main()
     char ch='+';
     printf("ASCII code of character %c is %d",ch,ch);
 }*/
-//3. Write a program to print size of an int, a float, a char and a double type variable.//
-#include<stdio.h>
+//3. Write a program to print size of an int, a float, a char and a double type variable.// 
+/*#include<stdio.h>
 int main()
 {
     int a;
@@ -30,5 +30,31 @@ int main()
     a=sizeof(double);
     printf("\nSize of float is %d",a);
     return 0;
-}
+}*/
+//4 Write a program to make the last digit of a number stored in variable as zero.Ex.2345 is 2340//
 
+/*#include<stdio.h>
+int main()
+{
+    int num,lastdigit;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    lastdigit=num-(num%10);
+    printf("Last digit of a number stored in variable is %d",lastdigit);
+    return 0;
+}*/
+/*5 Write a program to input a number from the user and also input a digit. Append a digit in 
+the number and print the resulting number.Ex.Number=234 and digit=9 resulting number =2349.*/
+
+#include<stdio.h>
+int main()
+{
+    int num,digit,resulting;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("Enter a digit:");
+    scanf("%d",&digit);
+    resulting=(num*10)+digit;
+    printf("Resulting Number is:%d",resulting);
+    return 0;
+}
