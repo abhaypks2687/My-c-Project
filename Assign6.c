9124b2c4fc1d3344d85b74558e15cf0272261d28
abhay shankar <abhayshnkr905@gmail.com>
2025-12-10 22:16:08 +0530

    Opertors 1.0

---
+++ Assign6.c
@@ -0,0 +1,50 @@
+//1.Assume a price of 1 USD is INR 84.23. WAP to to take the amount in INR and convert in USD.
+/*include<stdio.h>
+int main()
+{
+    int num;
+    double usd;
+    printf("Enter amount in INR:");
+    scanf("%d",&num);
+    usd=84.23*num;
+    printf("USD is %0.2lf",usd);
+    return 0;
+}*/
+/*//2.Write a program to take three digit number from the user and rotate its by digits by one position towrds right.//
+#include<stdio.h>
+int main()
+{
+    int num,num2;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    num2=(num%10)*100+num/10;
+    printf("%d",num2);
+    return 0;
+}*/
+//3.What will be the value stored in the variable x after executing follwing statement: x=10>8>4//
+/*#include<stdio.h>
+int main()
+{
+    int x;
+    x=10>8>4;
+    printf("After executing value will be stored in x= %d",x);
+    return 0;
+}*/
+//4.What will be the value stored in the variable x after executing follwing statement: x=!2>-2//
+/*#include<stdio.h>
+int main()
+{
+    int x;
+    x=!2>-2;
+    printf("After executing value will be stored in x=%d",x);
+    return 0;
+}*/
+//5.What will be value stored in the variable x after executing following statement:x=3<0&&5>0//
+#include<stdio.h>
+int main()
+{
+    int x;
+    x=3<0&&5>0;
+    printf("After executing value will be stored in x=%d",x);
+    return 0;
+}
