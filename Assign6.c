29ba9aed9c4c6790511f2abd29f6fda7329309cc
abhay shankar <abhayshnkr905@gmail.com>
2026-01-29 21:24:02 +0530

    INR convert into USD

--- Assign6.c
+++ Assign6.c
@@ -1,15 +1,14 @@
 //1.Assume a price of 1 USD is INR 84.23. WAP to to take the amount in INR and convert in USD.
-/*include<stdio.h>
+#include<stdio.h>
 int main()
 {
-    int num;
-    double usd;
+    float INR,USD;
     printf("Enter amount in INR:");
-    scanf("%d",&num);
-    usd=84.23*num;
-    printf("USD is %0.2lf",usd);
+    scanf("%f",&INR);
+    USD=INR/84.23;
+    printf("%0.2f INR==%0.2f USD",INR,USD);
     return 0;
-}*/
+}
 /*//2.Write a program to take three digit number from the user and rotate its by digits by one position towrds right.//
 #include<stdio.h>
 int main()
@@ -40,11 +39,11 @@ int main()
     return 0;
 }*/
 //5.What will be value stored in the variable x after executing following statement:x=3<0&&5>0//
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int x;
     x=3<0&&5>0;
     printf("After executing value will be stored in x=%d",x);
     return 0;
-}
+}*/
