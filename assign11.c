77504e8f8836fdf73aa59130fdc1021aec6b94a5
abhay shankar <abhayshnkr905@gmail.com>
2026-03-15 17:20:23 +0530

    assign11

---
+++ assign11.c
@@ -0,0 +1,121 @@
+//1.WAP a program to print MYSirg 5 times on the screen.//
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirg");
+        i++;
+    }
+    return 0;
+}*/
+//2.WAP a program to print the first 10 natural numbers.//
+/*#include<stdio.h>
+int main()
+{
+    int num=1;
+    while(num<=10)
+    {
+        printf("%d\n",num);
+        num++;
+    }
+    return 0;
+}*/
+//3.WAP a program to print the first 10 natural numbers in reverse order.//
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",11-i);
+    i++;
+    }
+    return 0;
+}*/
+//4.WAP a program to print the first 10 odd natural numbers.//
+/*#include<stdio.h>
+int main()
+{
+    int n=1;
+    while(n<=10)
+    {
+        printf("%d\n",2*n-1);
+        n++;
+    }
+    return 0;
+}*/
+//5.WAP a program to print the first 10 odd natural numbers in reverse order.//
+/*#include<stdio.h>
+int main()
+{
+    int num=1;
+    while(num<=10)
+    {
+        printf("%d\n",21-2*num);
+        num++;
+    }
+    return 0;
+}*/
+//6.WAP a program to print the first 10 even natural numbers //
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",2*i);
+        i++;
+    }
+}*/
+//7.WAP a program to print the first 10 even natural numbers in reverse order.//
+/*#include<stdio.h>
+int main()
+{
+    int num=1;
+    while(num<=10)
+    {
+      printf("%d\n",22-2*num);
+      num++;
+    }
+    return 0;
+}*/
+//8.WAP a program to print square of the first 10 natural numbers.//
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("Square of the Number %d is:%d\n",i,i*i);
+        i++;
+    }
+    return 0;
+}*/
+///9.WAP a program to print cubes of the first 10 natural numbers://
+/*#include<stdio.h>
+int main()
+{
+int num=1;
+while(num<=10)
+{
+    printf("Cubes of the number %d is %d\n",num,num*num*num);
+    num++;
+}
+return 0;
+}*/
+///9.WAP a program to print a table of 5.//
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("5*%d=%d\n",i,5*i);
+        i++;
+    }
+    return 0;
+}
+
+
